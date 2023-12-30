848920d0b1d9e3f5081c37cd338248d89bb4c411
Vivien <vchang103@gmail.com>
2023-12-29 17:02:44 -0800

    add loading bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-export default function Home() {
+
+const Home = (): JSX.Element => {
   return (
     <main className="flex h-dvh justify-center items-center">
       <div className="h-dvh w-dvw relative overflow-hidden border-solid border-4 border-black bg-yellow-300">
@@ -10,7 +11,12 @@ export default function Home() {
           height={160}
           width={160}
         />
+        <div className="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md border-slate-300 border-solid border-2 bg-slate-200 h-10 w-5/12 items-center text-center align-middle">
+          <p className="mt-1">Loading...</p>
+        </div>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
