d52c04bb57709a53cc93d749844f495ab5fd106f
Vivien <vchang103@gmail.com>
2024-01-04 20:01:47 -0800

    worked on trailing tail, bugs with mouse event and keeping the image with trailing tail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,51 @@
+"use client";
 import Image from "next/image";
+import { useRef } from "react";
+// import styles from '../styles/home.module.css'
+import styles from "../styles/home.module.css";
 
 const Home = (): JSX.Element => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const container = containerRef.current;
+  const img = imgRef.current;
+
+  const manageMouseMove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const { clientX, clientY } = event;
+
+    if (container && img) {
+      const containerPosition = container.getBoundingClientRect();
+      const x = clientX - containerPosition.x;
+      const y = clientY - containerPosition.y;
+      img.style.top = y + "px";
+      img.style.left = x + "px";
+      draw(x, y);
+    }
+  };
+
+  const draw = (x: number, y: number) => {
+    const div = document.createElement("div");
+    // const styleCircle =
+    //   "w-160 h-160 rounded-full border-solid border-4 border-black absolute -translate-x-1/2 -translate-y-1/2 z-1";
+    div.classList.add(styles["circle"]);
+    div.style.top = y + "px";
+    div.style.left = x + "px";
+    container?.append(div);
+  };
   return (
     <main className="flex h-dvh justify-center items-center">
-      <div className="h-dvh w-dvw relative overflow-hidden border-solid border-4 border-black bg-yellow-300">
+      <div
+        ref={containerRef}
+        onMouseMove={(e) => {
+          manageMouseMove(e);
+        }}
+        className="h-dvh w-dvw relative overflow-hidden border-solid border-4 border-black bg-yellow-300"
+      >
         <Image
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-2"
+          ref={imgRef}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-"
           src="/assets/smiley.svg"
           alt="smiley-face"
           height={160}
